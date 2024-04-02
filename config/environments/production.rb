21202dd70ff26dd058c2fff96bdde6f836ffdb96
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-02 12:09:56 +0900

    flyio デプロイ設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "vimemo.onrender.com"
+  config.hosts << "vimemoback.fly.dev"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
