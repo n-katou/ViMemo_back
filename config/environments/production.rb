c0b96b228b05213d09b89ece319944c3a858b67a
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-05 18:18:13 +0900

    production.rbの更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.hosts << "vimemoback.fly.dev"
+  config.require_master_key = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
