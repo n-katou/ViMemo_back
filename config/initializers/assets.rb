e2044ef1c6898428f4ae5a57a6beca56df453d19
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-06 03:40:41 +0900

    precompile設定

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,14 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.paths << Rails.root.join('app/assets/builds')
+
+# Rails.application.config.assets.paths << Rails.root.join('app/assets/builds')
+
+# 本番環境でのアセットのコピー先を指定する
+Rails.application.config.assets.precompile += %w( application.css application.js )
+
+# 本番環境でのアセットのコピー
+if Rails.env.production?
+  FileUtils.cp(Rails.root.join('assets/builds/application.css'), Rails.root.join('public/assets/application.css'))
+  FileUtils.cp(Rails.root.join('assets/builds/application.js'), Rails.root.join('public/assets/application.js'))
+end
