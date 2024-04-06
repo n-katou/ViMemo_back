1120eb8885ea8b6c2e8a59baf7a0a4c286285e49
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-07 04:26:36 +0900

    設定変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,24 +11,22 @@ Rails.application.configure do
 
   
   #本番環境用の設定
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    user_name: ENV['EMAIL_USER_NAME'],
-    password: ENV['EMAIL_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'gmail.com',
+  #   user_name: ENV['EMAIL_USER_NAME'],
+  #   password: ENV['EMAIL_PASSWORD'],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
   #開発環境用の設定
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.delivery_method = :letter_opener_web
-  # config.action_mailer.default_url_options = Settings.default_url_options.to_h
-  # config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.perform_deliveries = true
 
   # Do not eager load code on boot.
   config.eager_load = false
