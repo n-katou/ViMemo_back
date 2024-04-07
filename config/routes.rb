8239595409694c1338ba5c6fa21f01647a60281e
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-07 22:07:21 +0900

    googleログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
+  
 
   post 'oauth/callback', to: 'google_oauths#callback', as: :oauth_callback_post
   get 'oauth/callback', to: 'google_oauths#callback', as: :oauth_callback_get
