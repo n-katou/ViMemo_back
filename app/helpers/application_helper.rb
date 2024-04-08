41ef75648f87aee8848a97c598144bc96c8c2370
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-08 22:16:31 +0900

    youtube動画取得機能　実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def format_duration(seconds)
+    minutes = seconds / 60
+    seconds = seconds % 60
+    hours = minutes / 60
+    minutes = minutes % 60
+
+    format("%02d:%02d:%02d", hours, minutes, seconds)
+  end
 end
