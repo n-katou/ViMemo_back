37aad6de262920495a9a8d7f524dd680ee3eee3d
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-12 02:22:52 +0900

    容量制限

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,12 @@ class Video < ApplicationRecord
   mount_uploader :file_path, VideoUploader
   belongs_to :user
   has_many :notes
+
+  validate :image_size_validation
+
+  private
+
+  def image_size_validation
+    errors[:image] << "should be less than 5MB" if image.size > 5.megabytes
+  end
 end
