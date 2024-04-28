5f96228a75fbe5715abedd2223aac3afae7e41c2
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-28 13:20:04 +0900

    FE接続設定変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'http://front:4000'  # Next.jsがローカルで実行されている場合
-    origins 'https://vimemo.vercel.app'  # Next.jsがローカルで実行されている場合
+    origins 'http://front:4000', 'https://vimemo.vercel.app'
     resource '*',
     headers: :any,
     methods: [:get, :post, :options, :put, :delete],
