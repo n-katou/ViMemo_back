e4fa71e61ac7b5ebf2dd22eeb400778a7fab8bef
TISNT\tikK90316 <kato.naoto@tis.co.jp>
2024-05-02 13:20:58 +0900

    google_oauth_serviceの改善

--- app/services/google_oauth_service.rb
+++ app/services/google_oauth_service.rb
@@ -1,40 +1,48 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class GoogleOauthService
   def initialize(code)
     @code = code
     @client_id = ENV['GOOGLE_CLIENT_ID']
     @client_secret = ENV['GOOGLE_CLIENT_SECRET']
     @redirect_uri = "https://vimemo.fly.dev/oauth/callback?provider=google"
-    # @redirect_uri = "http://localhost:3000/oauth/callback?provider=google"
   end
 
   def authenticate
-    access_token = fetch_access_token(@code)
+    access_token = fetch_access_token
     user_info = fetch_user_info(access_token)
-    find_or_create_user(user_info)
+    find_or_create_user(user_info) if user_info
   end
 
   private
 
-  def fetch_access_token(code)
-    uri = URI.parse("https://oauth2.googleapis.com/token")
-    request = Net::HTTP::Post.new(uri)
-    request.content_type = "application/x-www-form-urlencoded"
-    request.set_form_data(code: code, client_id: @client_id, client_secret: @client_secret, redirect_uri: @redirect_uri, grant_type: "authorization_code")
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") { |http| http.request(request) }
+  def fetch_access_token
+    uri = URI("https://oauth2.googleapis.com/token")
+    response = Net::HTTP.post_form(uri, {
+      code: @code,
+      client_id: @client_id,
+      client_secret: @client_secret,
+      redirect_uri: @redirect_uri,
+      grant_type: "authorization_code"
+    })
     JSON.parse(response.body)["access_token"]
   end
 
   def fetch_user_info(access_token)
-    uri = URI.parse("https://www.googleapis.com/oauth2/v2/userinfo")
-    uri.query = URI.encode_www_form(access_token: access_token)
-    response = Net::HTTP.get_response(uri)
-    JSON.parse(response.body)
+    uri = URI("https://www.googleapis.com/oauth2/v2/userinfo")
+    request = Net::HTTP::Get.new(uri)
+    request['Authorization'] = "Bearer #{access_token}"
+    
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http| http.request(request) }
+    JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
   end
 
   def find_or_create_user(user_info)
     User.find_or_create_by(email: user_info['email']) do |user|
       user.name = user_info['name']
-      user.password = SecureRandom.hex(10)  # 例としてランダムパスワードを設定
+      user.password = SecureRandom.hex(10)
       user.password_confirmation = user.password
     end
   end
