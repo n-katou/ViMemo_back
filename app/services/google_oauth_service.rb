0d58175039e2a6dbfbaea22fc8078eb2977ffad0
TISNT\tikK90316 <kato.naoto@tis.co.jp>
2024-05-02 13:43:31 +0900

    google_oauth_serviceの改善

--- app/services/google_oauth_service.rb
+++ app/services/google_oauth_service.rb
@@ -1,10 +1,6 @@
-require 'net/http'
-require 'uri'
-require 'json'
-
 class GoogleOauthService
   attr_reader :error_message
-  
+
   def initialize(code)
     @code = code
     @client_id = ENV['GOOGLE_CLIENT_ID']
@@ -15,11 +11,15 @@ class GoogleOauthService
   def authenticate
     begin
       access_token = fetch_access_token
+      raise "アクセストークンが取得できませんでした" unless access_token
       user_info = fetch_user_info(access_token)
-      find_or_create_user(user_info) if user_info
+      raise "ユーザー情報が取得できませんでした" unless user_info
+      user = find_or_create_user(user_info)
+      raise "ユーザーの作成または検索に失敗しました" unless user
+      user
     rescue StandardError => e
       @error_message = e.message
-      Rails.logger.error("Google OAuthサービスでエラーが発生しました: #{e.message}")
+      Rails.logger.error("Google OAuthサービスでエラーが発生しました: #{e.message}, #{e.backtrace.join("\n")}")
       nil
     end
   end
@@ -35,7 +35,8 @@ class GoogleOauthService
       redirect_uri: @redirect_uri,
       grant_type: "authorization_code"
     })
-    JSON.parse(response.body)["access_token"]
+    result = JSON.parse(response.body)
+    result["access_token"]
   end
 
   def fetch_user_info(access_token)
@@ -44,14 +45,18 @@ class GoogleOauthService
     request['Authorization'] = "Bearer #{access_token}"
     
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http| http.request(request) }
-    JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
+    result = JSON.parse(response.body)
+    raise "Googleからのユーザー情報の取得に失敗しました" if result["error"]
+    result
   end
 
   def find_or_create_user(user_info)
-    User.find_or_create_by(email: user_info['email']) do |user|
+    user = User.find_or_create_by(email: user_info['email']) do |user|
       user.name = user_info['name']
       user.password = SecureRandom.hex(10)
       user.password_confirmation = user.password
     end
+    raise "データベースへのユーザー情報の保存に失敗しました" unless user.persisted?
+    user
   end
 end
