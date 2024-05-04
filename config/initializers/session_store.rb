bc301520361b2178d97fff2c3ca89ffb18607851
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-05 01:52:04 +0900

    sessionの設定変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: 'vimemo_session', httponly: true, secure: Rails.env.production?, same_site: :none
+# Rails.application.config.session_store :cookie_store, key: 'vimemo_session', httponly: true, secure: Rails.env.production?, same_site: :none
