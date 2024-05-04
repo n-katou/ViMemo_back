38964b525e1eeb165227c9baf9a70f2d04275abd
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-05 02:11:52 +0900

    sessionの設定変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,4 @@
-# Rails.application.config.session_store :cookie_store, key: 'vimemo_session', httponly: true, secure: Rails.env.production?, same_site: :none
+Rails.application.config.session_store :cookie_store, key: '_your_app_session',
+  httponly: true, # JavaScriptからのアクセスを防ぐ
+  secure: Rails.env.production?, # 本番環境でのみhttps接続時にクッキーを送信
+  same_site: :none # クロスオリジンのリクエストでクッキーを送信
