5b32fa2097412b7fc57b0372e770fb9ffcb301e8
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-06 04:25:13 +0900

    redis設定無効化

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
-$redis = Redis.new(
-  url: ENV['REDIS_URL'],
-  port: ENV['REDIS_PORT'] || 6379,
-  db: 0,
-  password: ENV['REDIS_PASSWORD']
-)
+# $redis = Redis.new(
+#   url: ENV['REDIS_URL'],
+#   port: ENV['REDIS_PORT'] || 6379,
+#   db: 0,
+#   password: ENV['REDIS_PASSWORD']
+# )
