e6ea2e40c4337ac281ca4491f87604e9619c1e8b
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-12 04:50:11 +0900

    ユーザー登録機能実装

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,57 +1,46 @@
 require 'jwt'
 require 'net/http'
+require 'openssl'
 
 module Api
   module V1
     class UsersController < ApiController
-
       def create
-        token = params[:firebase_token]
-        decoded_token = decode_firebase_token(token)
-
-        if decoded_token.nil?
-          render json: { error: 'Invalid token' }, status: :unauthorized
-          return
-        end
-
-        user_data = decoded_token[:claims]  # IDトークンのクレーム部分にユーザー情報が含まれています。
-        @user = User.find_or_create_by_uid(user_data)
-
-        if @user.persisted?
-          render json: { token: @user.auth_token, user: @user }, status: :created
+        user = User.find_by(email: user_params[:email])
+    
+        if user
+          render json: { error: 'User already exists with this email address.' }, status: :unprocessable_entity
         else
-          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-        end
-      end
-
-      def logout
-        if current_user.update(auth_token: nil)  # 現在のユーザーのauth_tokenをクリア
-          render json: { message: "Logged out successfully." }, status: :ok
-        else
-          render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+          user = User.new(user_params)
+          if user.save
+            render json: user, status: :created
+          else
+            render json: user.errors, status: :unprocessable_entity
+          end
         end
       end
 
       private
 
-      def decode_firebase_token(token)
-        jwks_uri = URI("https://www.googleapis.com/service_accounts/v1/jwk/securetoken@system.gserviceaccount.com")
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+
+      def decode_unverified(token)
+        JWT.decode(token, nil, false) # 第三引数にfalseを指定して検証をスキップ
+      end
+
+      def fetch_public_key(kid)
+        jwks_uri = URI("https://www.googleapis.com/oauth2/v3/certs")
         jwks_raw = Net::HTTP.get(jwks_uri)
-        jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
-        decoded_token = nil
+        jwks_keys = JSON.parse(jwks_raw)['keys']
+        jwk = jwks_keys.find { |key| key['kid'] == kid }
 
-        jwks_keys.each do |key|
-          next unless key['kid'] == decoded_token.header['kid']
-
-          public_key = OpenSSL::X509::Certificate.new(Base64.decode64(key['x5c'].first)).public_key
-          decoded_token = JWT.decode(token, public_key, true, { algorithm: 'RS256', verify_iat: true })
-          break
-        rescue JWT::VerificationError, JWT::DecodeError => e
-          Rails.logger.error "Token verification failed: #{e.message}"
-          return nil
+        if jwk.nil?
+          raise 'Public key not found.'
         end
 
-        decoded_token
+        OpenSSL::X509::Certificate.new(Base64.decode64(jwk['x5c'].first)).public_key
       end
     end
   end
