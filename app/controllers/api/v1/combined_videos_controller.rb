91f2bb2cba6e98202d6b7068e3213921fcabb181
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-21 03:31:52 +0900

    いいね解除機能（ボタン切り替わり不可）

--- app/controllers/api/v1/combined_videos_controller.rb
+++ app/controllers/api/v1/combined_videos_controller.rb
@@ -1,38 +1,50 @@
 module Api
   module V1
     class CombinedVideosController < ApiController
+      before_action :authenticate_user!
+
       def favorites
         @user = current_user
         @likes = @user.likes.includes(:likeable)
-      
-        # 'likeable_type' を基にして異なるタイプのビデオをフィルタリング
-        liked_youtube_videos = @likes.select { |like| like.likeable_type == "YoutubeVideo" }.map(&:likeable)
-      
+
+        liked_youtube_videos = @likes.where(likeable_type: "YoutubeVideo").map(&:likeable)
+
         sort_key = params[:sort_key] || 'created_at'
         sort_order = params[:sort_order] || 'desc'
-        
-        # ソート処理
+
         liked_youtube_videos.sort_by! { |video| video.send(sort_key) }
         liked_youtube_videos.reverse! if sort_order == 'desc'
-      
-        # ページネーション
+
         @paginated_videos = Kaminari.paginate_array(liked_youtube_videos).page(params[:page]).per(9)
-        
+
         render json: {
-          videos: @paginated_videos,
+          videos: @paginated_videos.map { |video|
+            {
+              id: video.id,
+              title: video.title,
+              description: video.description,
+              published_at: video.published_at,
+              youtube_id: video.youtube_id,
+              duration: video.duration,
+              likes_count: video.likes_count,
+              notes_count: video.notes_count,
+              user: {
+                id: video.user.id,
+                name: video.user.name,
+                avatar: video.user.avatar.url || "#{ENV['S3_BASE_URL']}/default-avatar.jpg"
+              },
+              likes: video.likes.map { |like|
+                { id: like.id, user_id: like.user_id, likeable_id: like.likeable_id, likeable_type: like.likeable_type }
+              }
+            }
+          },
           pagination: {
             current_page: @paginated_videos.current_page,
             total_pages: @paginated_videos.total_pages,
             next_page: @paginated_videos.next_page,
             prev_page: @paginated_videos.prev_page
           }
-        }
-      end
-
-      private
-      
-      def search_params
-        params.fetch(:q, {}).permit(:title_cont, :created_at, :likes_count, :notes_count, :sort_key, :sort_order)
+        }, status: :ok
       end
     end
   end
