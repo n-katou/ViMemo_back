18333c71303c5df5db78654820bf5aabd785ab67
n-katou <102382177+n-katou@users.noreply.github.com>
2024-05-28 23:48:58 +0900

    domain変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://front:4000', 'http://localhost:4000', 'https://vimemo.vercel.app', 'https://vimemo.fly.dev', 'https://vimemo-63237.firebaseapp.com'
+    origins 'http://front:4000', 'http://localhost:4000', 'https://vimemo.vercel.app', 'https://vimemo.fly.dev', 'https://vimemo-63237.firebaseapp.com', 'https://vi-memo.com'
     resource '*',
     headers: :any,
     methods: [:get, :post, :patch, :put, :delete, :options, :head],
