fd105c8207c6d4cdd61b50ba8851f8677e96a665
n-katou <102382177+n-katou@users.noreply.github.com>
2024-06-03 22:49:00 +0900

    youtubeのテスト

--- spec/factories/youtube_videos.rb
+++ spec/factories/youtube_videos.rb
@@ -1,12 +1,16 @@
-# spec/factories/youtube_videos.rb
-
 FactoryBot.define do
   factory :youtube_video do
     title { "Sample Video" }
     description { "Sample Description" }
     published_at { Time.now }
-    youtube_id { "dQw4w9WgXcQ" }
+    sequence(:youtube_id) { |n| "dQw4w9WgXcQ#{n}" }
     duration { 600 }
+    likes_count { 0 }
+    notes_count { 0 }
     association :user
+
+    after(:create) do |youtube_video|
+      create_list(:note, 2, youtube_video: youtube_video, user: youtube_video.user)
+    end
   end
 end
--- spec/requests/api/v1/youtube_videos_spec.rb
+++ spec/requests/api/v1/youtube_videos_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe "YoutubeVideos", type: :request do
     let!(:user) { create(:user) }
     let!(:youtube_videos) { create_list(:youtube_video, 3, user: user) }
 
+    before do
+      youtube_videos.each do |video|
+        create_list(:note, 2, youtube_video: video, user: user)
+        create_list(:like, 2, likeable: video, user: user)
+      end
+    end
+
     it "returns a success response" do
       get api_v1_youtube_videos_path
       expect(response).to have_http_status(:success)
@@ -25,6 +32,41 @@ RSpec.describe "YoutubeVideos", type: :request do
       expect(video['description']).to eq(youtube_videos.first.description)
       expect(video['published_at'].to_time.utc).to be_within(1.second).of(youtube_videos.first.published_at.utc)
       expect(video['duration']).to eq(youtube_videos.first.duration)
+    end
+
+    it "returns videos sorted by likes" do
+      youtube_videos.each_with_index do |video, index|
+        create_list(:like, index + 1, likeable: video, user: user)
+      end
+
+      get api_v1_youtube_videos_path, params: { sort: 'likes_desc' }
+      expect(response).to have_http_status(:success)
+      sorted_videos = json['videos']
+      expect(sorted_videos.first['likes_count']).to be >= sorted_videos.second['likes_count']
+    end
+
+    it "returns videos filtered by title" do
+      filtered_title = youtube_videos.first.title
+      get api_v1_youtube_videos_path, params: { q: { title_cont: filtered_title } }
+      expect(response).to have_http_status(:success)
+      filtered_videos = json['videos']
+      expect(filtered_videos.count { |video| video['title'].include?(filtered_title) }).to eq(1)
+    end
+
+    it "returns videos with user information" do
+      get api_v1_youtube_videos_path
+      expect(response).to have_http_status(:success)
+      video = json['videos'].first
+      expect(video['user']['id']).to eq(user.id)
+      expect(video['user']['name']).to eq(user.name)
+    end
+
+    it "returns videos with notes" do
+      get api_v1_youtube_videos_path
+      expect(response).to have_http_status(:success)
+      video = json['videos'].first
+      expect(video['notes']).not_to be_nil
+      expect(video['notes'].length).to eq(2)
     end
   end
 end
