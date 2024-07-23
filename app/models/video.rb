7d99b07fb2429c7acfa5ea97942e19c6fffcc5aa
n-katou <102382177+n-katou@users.noreply.github.com>
2024-07-23 17:45:39 +0900

    refactor: Modelメソッドの整理

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,14 +7,4 @@ class Video < ApplicationRecord
   validate :image_size_validation
 
   paginates_per 10
-
-  private
-
-  def image_size_validation
-    errors[:image] << "should be less than 5MB" if image.size > 5.megabytes
-  end
-
-  def liked_by?(user)
-    likes.where(user: user).exists?
-  end
 end
--- app/models/youtube_video.rb
+++ app/models/youtube_video.rb
@@ -4,13 +4,4 @@ class YoutubeVideo < ApplicationRecord
   belongs_to :user
 
   paginates_per 10
-  
-  def embed_url
-    video_id = self.url.split('=')[1]
-    "https://www.youtube.com/embed/#{video_id}"
-  end
-  
-  def liked_by?(user)
-    likes.where(user: user).exists?
-  end
 end
