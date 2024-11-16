80f271542e4d479ffed3dfbaa2245c4c778994ee
Naoto Kato <naoto.light@gmail.com>
2024-11-17 01:31:48 +0900

    Update puma.rb
    fly対策

--- config/puma.rb
+++ config/puma.rb
@@ -4,9 +4,21 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+
+#一度コメントアウト
+# max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+# threads min_threads_count, max_threads_count
+
+#新規で追記(fly.io crash対策)
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS", 1).to_i
 threads min_threads_count, max_threads_count
+
+workers ENV.fetch("WEB_CONCURRENCY", 1).to_i  # 1ワーカーに制限
+
+preload_app!
+#新規で追記(fly.io crash対策)
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
